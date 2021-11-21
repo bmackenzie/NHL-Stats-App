f14ec737a8da358b516d051f41f8906ae58118f8
bmackenzie <39527613+bmackenzie@users.noreply.github.com>
2021-11-20 18:44:11 -0600

    Added Team Selection
    Added functionality for choosing the team and year you want to see visualizations for.

--- app.py
+++ app.py
@@ -7,22 +7,27 @@ import requests
 
 import pandas as pd
 
+## TODO: make sure table updates, figure out why year options , update the for years in year part of the df building loop.  Change it so that for each team it grabs every year past the starting year that appears in the api summary for their team
+
 ids = ['1','2','3','4','5','6','7','8','9','10','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','28','29','30']
 
 years = ['20192020', '20182019', '20172018', '20162017', '20152016', '20142015', '20132014', '20122013', '20112012', '20102011']
-years_slider = [2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011]
 
 df = pd.DataFrame(columns =['gamesPlayed', 'wins', 'losses', 'ot', 'pts', 'ptPctg', 'goalsPerGame', 'goalsAgainstPerGame', 'evGGARatio', 'powerPlayPercentage', 'powerPlayGoals', 'powerPlayGoalsAgainst', 'powerPlayOpportunities', 'penaltyKillPercentage', 'shotsPerGame', 'shotsAllowed', 'winScoreFirst', 'winOppScoreFirst', 'winLeadFirstPer', 'winLeadSecondPer', 'winOutshootOpp', 'winOutshotByOpp', 'faceOffsTaken', 'faceOffsWon', 'faceOffsLost', 'faceOffWinPercentage', 'shootingPctg', 'savePctg', 'Team'] )
-for year in years:
-    teams = requests.get('https://statsapi.web.nhl.com/api/v1/teams/4/?expand=team.stats&season='+year)
+for i in ids:
+    for year in years:
+        teams = requests.get('https://statsapi.web.nhl.com/api/v1/teams/' + i +'/?expand=team.stats&season='+year)
 
-    team_name = teams.json()['teams'][0]['name']
+        team_name = teams.json()['teams'][0]['name']
 
-    team_stats = teams.json()['teams'][0]['teamStats'][0]['splits'][0]['stat']
+        team_stats = teams.json()['teams'][0]['teamStats'][0]['splits'][0]['stat']
 
-    team_stats['Year'] = year[-4:]
+        team_stats['Year'] = year[-4:]
+        team_stats['Team'] = team_name
 
-    df = df.append(team_stats, ignore_index = True)
+        df = df.append(team_stats, ignore_index = True)
+
+df['Year'] = pd.to_numeric(df['Year'])
 
 df['Year'] = pd.to_numeric(df['Year'])
 
@@ -43,50 +48,43 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Dropdown(id='input-team',
             options=[
-                {'label': 'New Jersey Devils', 'value': 1},
-                {'label': 'New York Islanders', 'value': 2},
-                {'label': 'New York Rangers', 'value': 3},
-                {'label': 'Philadelphia Flyers', 'value': 4},
-                {'label': 'Pittsburgh Penguins', 'value': 5},
-                {'label': 'Boston Bruins', 'value': 6},
-                {'label': 'Buffalo Sabres', 'value': 7},
-                {'label': 'Montreal Canadiens', 'value': 8},
-                {'label': 'Ottawa Senators', 'value': 9},
-                {'label': 'Toronto Maple Leafs', 'value': 10},
-                {'label': 'Carolina Hurricanes', 'value': 12},
-                {'label': 'Florida Panthers', 'value': 13},
-                {'label': 'Tampa Bay Lightning', 'value': 14},
-                {'label': 'Washington Capitals', 'value': 15},
-                {'label': 'Chicago Blackhawks', 'value': 16},
-                {'label': 'Detroit Red Wings', 'value': 17},
-                {'label': 'Nashville Predators', 'value': 18},
-                {'label': 'St. Louis Blues', 'value': 19},
-                {'label': 'Calgary Flames', 'value': 20},
-                {'label': 'Colorado Avalanche', 'value': 21},
-                {'label': 'Edmonton Oilers', 'value': 22},
-                {'label': 'Vancouver Canucks', 'value': 23},
-                {'label': 'Anaheim Ducks', 'value': 24},
-                {'label': 'Dallas Stars', 'value': 25},
-                {'label': 'Los Angeles Kings', 'value': 26},
-                {'label': 'San Jose Sharks', 'value': 28},
-                {'label': 'Columbus Blue Jackets', 'value': 29},
-                {'label': 'Minnesota Wild', 'value': 30},
-                {'label': 'Winnipeg Jets', 'value': 52},
-                {'label': 'Arizona Coyotes', 'value': 53},
-                {'label': 'Vegas Golden Knights', 'value': 54},
-                {'label': 'Seattle Kraken', 'value': 55},
+                {'label': 'New Jersey Devils', 'value': 'New Jersey Devils'},
+                {'label': 'New York Islanders', 'value': 'New York Islanders'},
+                {'label': 'New York Rangers', 'value': 'New York Rangers'},
+                {'label': 'Philadelphia Flyers', 'value': 'Philadelphia Flyers'},
+                {'label': 'Pittsburgh Penguins', 'value': 'Pittsburgh Penguins'},
+                {'label': 'Boston Bruins', 'value': 'Boston Bruins'},
+                {'label': 'Buffalo Sabres', 'value': 'Buffalo Sabres'},
+                {'label': 'Montréal Canadiens', 'value': 'Montréal Canadiens'},
+                {'label': 'Ottawa Senators', 'value': 'Ottawa Senators'},
+                {'label': 'Toronto Maple Leafs', 'value': 'Toronto Maple Leafs'},
+                {'label': 'Carolina Hurricanes', 'value': 'Carolina Hurricanes'},
+                {'label': 'Florida Panthers', 'value': 'Florida Panthers'},
+                {'label': 'Tampa Bay Lightning', 'value': 'Tampa Bay Lightning'},
+                {'label': 'Washington Capitals', 'value': 'Washington Capitals'},
+                {'label': 'Chicago Blackhawks', 'value': 'Chicago Blackhawks'},
+                {'label': 'Detroit Red Wings', 'value': 'Detroit Red Wings'},
+                {'label': 'Nashville Predators', 'value': 'Nashville Predators'},
+                {'label': 'St. Louis Blues', 'value': 'St. Louis Blues'},
+                {'label': 'Calgary Flames', 'value': 'Calgary Flames'},
+                {'label': 'Colorado Avalanche', 'value': 'Colorado Avalanche'},
+                {'label': 'Edmonton Oilers', 'value': 'Edmonton Oilers'},
+                {'label': 'Vancouver Canucks', 'value': 'Vancouver Canucks'},
+                {'label': 'Anaheim Ducks', 'value': 'Anaheim Ducks'},
+                {'label': 'Dallas Stars', 'value': 'Dallas Stars'},
+                {'label': 'Los Angeles Kings', 'value': 'Los Angeles Kings'},
+                {'label': 'San Jose Sharks', 'value': 'San Jose Sharks'},
+                {'label': 'Columbus Blue Jackets', 'value': 'Columbus Blue Jackets'},
+                {'label': 'Minnesota Wild', 'value': 'Minnesota Wild'},
+                {'label': 'Winnipeg Jets', 'value': 'Winnipeg Jets'},
+                {'label': 'Arizona Coyotes', 'value': 'Arizona Coyotes'},
+                {'label': 'Vegas Golden Knights', 'value': 'Vegas Golden Knights'},
+                {'label': 'Seattle Kraken', 'value': 'Seattle Kraken'},
                 ],
             placeholder='Select a Team',
-            value = 4,
+            value = 'Philadelphia Flyers',
             style={'width':'80%','padding':'3px','font-size':'20px','text-align':'center'}),
-    dcc.Dropdown(id='input-year',
-            options=[
-                {'label': '2011', 'value': 2011},
-                {'label': '2012', 'value':2012}
-                ],
-            placeholder='Select a Year',
-            value = 2011,
-            style={'width':'80%','padding':'3px','font-size':'20px','text-align':'center'}),
+    dcc.Dropdown(id ='year-options'),
     html.Div([
         dcc.Graph(id='graph-with-slider'),
     ]),
@@ -95,13 +93,26 @@ app.layout = html.Div([
     ])
 ])
 
+@app.callback(
+        Output('year-options', 'options'),
+        Input('input-team', 'value'),
+)
+def set_year_options(team_name):
+    filtered_df = df[df['Team'] == 'Philadelphia Flyers']
+    years_dict = {}
+    for year in filtered_df['Year']:
+        years_dict[year] = year
+    return[{'label':i, 'value':i} for i in years_dict]
+
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    Input('input-year', 'value'),
-    Input('input-team', 'value'))
+    Input('year-options', 'value'),
+    Input('input-team', 'value'),
+    )
 def update_figure(selected_year, team):
-    filtered_df = df[(df['Year']>= (int(selected_year) - 2)) & (df['Year']<= (int(selected_year) + 2))]
+    filtered_df = df[df['Team'] == team]
+    filtered_df = filtered_df[(filtered_df['Year']>= (int(selected_year) - 2)) & (df['Year']<= (int(selected_year) + 2))]
 
     fig = px.line(filtered_df, x="Year", y =["goalsPerGame", 'goalsAgainstPerGame'] )
 
